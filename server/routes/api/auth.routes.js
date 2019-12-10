a9005e126ac8c28138c3292081c99826ef6eeee5
María Simó <maria@coponstudio.es>
2019-12-10 11:12:27 +0100

    Added edit user info route at server end

--- server/routes/api/auth.routes.js
+++ server/routes/api/auth.routes.js
@@ -1,36 +1,39 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../../models/User");
+
 // const uploader = require('../../configs/cloudinary.config')
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+const salt = bcrypt.genSaltSync(bcryptSalt);
 
+router.post("/signup", (req, res, next) => {
+  const { username, password, picture } = req.body;
 
-router.post('/signup', (req, res, next) => {
-  const { username, password, picture } = req.body
-  console.log("hola")
   if (!username || !password) {
-    res.status(400).json({ message: 'Provide username and password' });
+    res.status(400).json({ message: "Provide username and password" });
     return;
   }
 
   if (password.length < 2) {
-    res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
+    res.status(400).json({
+      message:
+        "Please make your password at least 8 characters long for security purposes."
+    });
     return;
   }
 
   User.findOne({ username }, (err, foundUser) => {
-
     if (err) {
       res.status(500).json({ message: "Username check went bad." });
       return;
     }
 
     if (foundUser) {
-      res.status(400).json({ message: 'Username taken. Choose another one.' });
+      res.status(400).json({ message: "Username taken. Choose another one." });
       return;
     }
 
@@ -45,16 +48,17 @@ router.post('/signup', (req, res, next) => {
 
     newUser.save(err => {
       if (err) {
-        res.status(400).json({ message: 'Saving user to database went wrong.' });
+        res
+          .status(400)
+          .json({ message: "Saving user to database went wrong." });
         return;
       }
 
       // Automatically log in user after sign up
       // .login() here is actually predefined passport method
-      req.login(newUser, (err) => {
-
+      req.login(newUser, err => {
         if (err) {
-          res.status(500).json({ message: 'Login after signup went bad.' });
+          res.status(500).json({ message: "Login after signup went bad." });
           return;
         }
 
@@ -66,12 +70,12 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-
-
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, theUser, failureDetails) => {
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
     if (err) {
-      res.status(500).json({ message: 'Something went wrong authenticating user' });
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
       return;
     }
 
@@ -83,9 +87,9 @@ router.post('/login', (req, res, next) => {
     }
 
     // save user in session
-    req.login(theUser, (err) => {
+    req.login(theUser, err => {
       if (err) {
-        res.status(500).json({ message: 'Session save went bad.' });
+        res.status(500).json({ message: "Session save went bad." });
         return;
       }
       // We are now logged in (that's why we can also send req.user)
@@ -94,20 +98,35 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
-router.post('/logout', (req, res, next) => {
+router.post("/logout", (req, res, next) => {
   // req.logout() is defined by passport
   req.logout();
-  res.status(200).json({ message: 'Log out success!' });
+  res.status(200).json({ message: "Log out success!" });
 });
 
-
-router.get('/loggedin', (req, res, next) => {
+router.get("/loggedin", (req, res, next) => {
   // req.isAuthenticated() is defined by passport
   if (req.isAuthenticated()) {
     res.status(200).json(req.user);
     return;
   }
-  res.status(403).json({ message: 'Unauthorized' });
+  res.status(403).json({ message: "Unauthorized" });
+});
+
+
+router.post("/edit/:id", (req, res, next) => {
+  const { id } = req.params;
+  User.findByIdAndUpdate(
+    id,
+    {
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password, salt)
+    },
+    { new: true }
+  ).then(userUpdated => {
+    res.status(200).json(userUpdated);
+  })
+  .catch(err => res.json(err))
 });
 
 // router.post('/upload', uploader.single('picture'), (req, res) => {
