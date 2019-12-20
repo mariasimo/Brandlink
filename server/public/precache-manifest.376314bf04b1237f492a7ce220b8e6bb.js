0d4103dbdf9a65c6c44f84e4d78f9365233e95bd
María Simó <maria@coponstudio.es>
2019-12-20 16:50:55 +0100

    fixed

---
+++ server/public/precache-manifest.376314bf04b1237f492a7ce220b8e6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363a181691d58a3cc540e1a876f3497d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b032db879c6c176fd8a9",
+    "url": "/static/css/main.489fd95f.chunk.css"
+  },
+  {
+    "revision": "955cc4ba5af2d814506c",
+    "url": "/static/js/2.b6860862.chunk.js"
+  },
+  {
+    "revision": "7b23692a2b88dd938b18bee3964974cb",
+    "url": "/static/js/2.b6860862.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b032db879c6c176fd8a9",
+    "url": "/static/js/main.a2d9f365.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
